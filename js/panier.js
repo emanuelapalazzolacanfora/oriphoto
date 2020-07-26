403b0cb7a0b78b4b4e5da0fc0745d19bd23791c6
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-26 18:24:05 +0200

    Update panier.js

--- js/panier.js
+++ js/panier.js
@@ -139,6 +139,7 @@ form.addEventListener('submit', (e) => {
     }
     post("https://oc-p5-api.herokuapp.com/api/cameras/order", data).then((response) => {
         window.location = `./confirmation-de-commande.html?id=${response.orderId}&user=${response.contact.firstName}`; // Redirige vers la page de confirmation de commande
+        localStorage.clear();
     })
 });
 
