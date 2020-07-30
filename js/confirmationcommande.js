7b54b82a6dde9ae43d951e40691cdcce9aa81276
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-30 20:24:11 +0200

    Update confirmationcommande.js

--- js/confirmationcommande.js
+++ js/confirmationcommande.js
@@ -4,5 +4,5 @@ const userName = params.get('user')
 const price = params.get('price')
 
 document.querySelector(".order-id").textContent = orderId;
-document.querySelector(".price").textContent = price
-document.querySelector(".user").textContent = userName
+document.querySelector(".price").textContent = price;
+document.querySelector(".user").textContent = userName;
