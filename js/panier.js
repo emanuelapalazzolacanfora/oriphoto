6ca4b70339b6ee639d2939e876d0e07d90936f8c
Emanuela Palazzo Lacanfora <62099675+emanuelapalazzolacanfora@users.noreply.github.com>
2020-07-30 19:21:28 +0200

    Update panier.js

--- js/panier.js
+++ js/panier.js
@@ -136,10 +136,20 @@ form.addEventListener('submit', (e) => {
             address: e.target.adresseclient.value,
             city: e.target.villeclient.value
         },
-        products
+        products,
+        let totalPrice = 0;
+
+        const updateTotalPrice = () => {
+            const panier = JSON.parse(localStorage.getItem('panier'))
+            let totalPrice = 0;
+            for (let product of Object.values(panier)) {
+                 totalPrice += product.price * product.quantity
+            }
+    
+        }
     }
     post("https://oc-p5-api.herokuapp.com/api/cameras/order", data).then((response) => {
-        window.location = `./confirmation-de-commande.html?id=${response.orderId}&user=${response.contact.firstName}&price=${response.panier.totalPrice}`; // Redirige vers la page de confirmation de commande
+        window.location = ./confirmation-de-commande.html?id=${response.orderId}&user=${response.contact.firstName}&price=${response.panier.totalPrice}; // Redirige vers la page de confirmation de commande
         localStorage.clear();
     })
 });
